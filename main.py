15895fca67392ffa3d96daa6942db13970e11725
Alban343 <bauduin.alban@gmail.com>
2024-03-27 19:22:55 +0100

    modified: main.py

--- main.py
+++ main.py
@@ -16,31 +16,39 @@ class MyHTMLParser(HTMLParser):
         for attribut in attrs:
             if attribut[0] == 'class' and attribut[1] == 'notes':
                 self.notes_content = True
-                print(tag)
+                # print(tag)
             
 # Fonction qui définit la zone de fin de récupération des données (réinitialisation de mon boulean notes_content)
     def handle_endtag(self, tag):
         if tag == "div" and self.notes_content == True:
-            print("Encountered an end tag :", tag)
+            # print("Encountered an end tag :", tag)
             self.notes_content = False
 
 # Récupération des caractères
     def handle_data(self, data):
         if self.notes_content == True:
-            print("Encountered some data  :", data)
+            print(data)
 
 # Fonction principale
 def main():
-    try:
-        # Appel des données
-        test = requests.get("https://thesession.org/tunes/1")
-        # Appel du parser
-        parser = MyHTMLParser()
-        # Envoi des données dans le parser
-        parser.feed(test.text)
+    n = 0
+    previous_url = None
 
-    except ValueError as ve:
-        return str(ve)
+    while True:
+        url = "https://thesession.org/tunes/" + str(n)
+
+        try:
+        # Appel des données
+            test = requests.get(url)
+        # Appel du parser
+            parser = MyHTMLParser()
+        # Envoi des données dans le parser
+            parser.feed(test.text)
+            previous_url = url
+            n = n+1
+
+        except ValueError as ve:
+            return str(ve)
 
 if __name__ == "__main__":
     sys.exit(main())
